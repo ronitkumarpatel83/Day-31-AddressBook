7e16027cca4a285601197e0f8d12150c111caf9a
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-05-03 10:39:13 +0530

    [Ronit] Added : Insert address book contacts

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -12,3 +12,10 @@ Zip int,
 Phone bigint,
 Email varchar(200)
 );
+-------------UC-3---------------
+INSERT INTO addressbook (First_Name,Last_Name,Address,City,State,Zip,Phone,Email)
+VALUES ('Ronit','Patel','SNG','Sundergarh','Odisha',770001,7008427274,'ronitkumarpatel83@gmail.com'),
+('Dibya','Jena','KJR','Keonjhar','Meghalaya',123456,1234567890,'dibyajena@gmail.com'),
+('Aman','Patel','JSD','Jharsuguda','Bihar',236549,2345678910,'amanpatel@gmail.com'),
+('Durgesh','Dash','PatraPada','Bangalore','Karnatak',345612,3654789120,'durgeshdash@gmail.com'),
+('Amisha','Patel','Bhor','Pune','Maharastra',456123,4567891230,'amisha@gmail.com');
