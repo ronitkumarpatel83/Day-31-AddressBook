dd9b37be400f65f77f633fc3adbae934781405db
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-05-03 10:20:27 +0530

    [Ronit] Added : Create address book table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,14 @@
 create database address_book_service_DB;
 use address_book_service_DB;
-
+----------UC-2----------------
+CREATE TABLE addressbook (
+Serial_Number int identity(1,1) primary key,
+First_Name varchar(200),
+Last_Name varchar(200),
+Address varchar(200),
+City varchar(200),
+State varchar(200),
+Zip int,
+Phone bigint,
+Email varchar(200)
+);
